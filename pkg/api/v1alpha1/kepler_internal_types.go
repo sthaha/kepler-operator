23be3c5d0a95b5fea71f9b345a526fbed6526275
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2023-12-07 13:04:23 +0900

    add model-server/estimator to KeplerInternal
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

--- pkg/api/v1alpha1/kepler_internal_types.go
+++ pkg/api/v1alpha1/kepler_internal_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -53,8 +54,73 @@ type OpenShiftSpec struct {
 
 // KeplerInternalSpec defines the desired state of KeplerInternal
 type KeplerInternalSpec struct {
-	Exporter  InternalExporterSpec `json:"exporter"`
-	OpenShift OpenShiftSpec        `json:"openshift,omitempty"`
+	Exporter    InternalExporterSpec     `json:"exporter"`
+	Estimator   *InternalEstimatorSpec   `json:"estimator,omitempty"`
+	ModelServer *InternalModelServerSpec `json:"modelServer,omitempty"`
+	OpenShift   OpenShiftSpec            `json:"openshift,omitempty"`
+}
+
+// Kepler Model Server Spec
+type InternalModelServerSpec struct {
+
+	// +kubebuilder:default=false
+	Enabled bool `json:"enabled,omitempty"`
+
+	Image string `json:"image,omitempty"`
+
+	// +kubebuilder:default=""
+	URL string `json:"url,omitempty"`
+
+	// +kubebuilder:default=8100
+	// +kubebuilder:validation:Maximum=65535
+	// +kubebuilder:validation:Minimum=1
+	Port int `json:"port,omitempty"`
+
+	// +kubebuilder:default=""
+	Path string `json:"path,omitempty"`
+
+	// +kubebuilder:default=""
+	RequestPath string `json:"requestPath,omitempty"`
+
+	// +kubebuilder:default=""
+	ListPath string `json:"listPath,omitempty"`
+
+	// +kubebuilder:default=""
+	PipelineURL string `json:"pipelineUrl,omitempty"`
+
+	// +kubebuilder:default=""
+	ErrorKey string `json:"errKey,omitempty"`
+
+	Storage ModelServerStorageSpec `json:"storage,omitempty"`
+}
+
+type ModelServerStorageSpec struct {
+	PersistentVolumeClaim *corev1.PersistentVolumeClaimSpec `json:"persistentVolumeClaim,omitempty"`
+}
+
+// Estimator Spec
+type InternalEstimatorSpec struct {
+	Image     string         `json:"image,omitempty"`
+	Node      EstimatorGroup `json:"node,omitempty"`
+	Container EstimatorGroup `json:"container,omitempty"`
+}
+
+type EstimatorGroup struct {
+	Total      *EstimatorConfig `json:"total,omitempty"`
+	Components *EstimatorConfig `json:"components,omitempty"`
+}
+
+type EstimatorConfig struct {
+	// +kubebuilder:default=false
+	SidecarEnabled bool `json:"sidecar,omitempty"`
+
+	InitUrl  string             `json:"initUrl,omitempty"`
+	Selector *ModelSelectorSpec `json:"selector,omitempty"`
+}
+
+type ModelSelectorSpec struct {
+	ModelName        string `json:"modelName,omitempty"`
+	FilterConditions string `json:"filterConditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -64,13 +130,15 @@ type KeplerInternalSpec struct {
 // +kubebuilder:printcolumn:name="Port",type=integer,JSONPath=`.spec.exporter.deployment.port`
 // +kubebuilder:printcolumn:name="Desired",type=integer,JSONPath=`.status.exporter.desiredNumberScheduled`
 // +kubebuilder:printcolumn:name="Current",type=integer,JSONPath=`.status.exporter.currentNumberScheduled`
-// +kubebuilder:printcolumn:name="Up-to-date",type=integer,JSONPath=`.status.updatedNumberScheduled`
+// +kubebuilder:printcolumn:name="Up-to-date",type=integer,JSONPath=`.status.exporter.updatedNumberScheduled`
 // +kubebuilder:printcolumn:name="Ready",type=integer,JSONPath=`.status.exporter.numberReady`
 // +kubebuilder:printcolumn:name="Available",type=integer,JSONPath=`.status.exporter.numberAvailable`
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="Image",type=string,JSONPath=`.spec.exporter.deployment.image`
 // +kubebuilder:printcolumn:name="Node-Selector",type=string,JSONPath=`.spec.exporter.deployment.nodeSelector`,priority=10
 // +kubebuilder:printcolumn:name="Tolerations",type=string,JSONPath=`.spec.exporter.deployment.tolerations`,priority=10
+// +kubebuilder:printcolumn:name="Estimator",type=string,JSONPath=`.status.estimator.status`
+// +kubebuilder:printcolumn:name="Model-Server",type=string,JSONPath=`.status.modelServer.status`
 //
 // KeplerInternal is the Schema for the keplers internal API
 type KeplerInternal struct {
@@ -81,9 +149,27 @@ type KeplerInternal struct {
 	Status KeplerInternalStatus `json:"status,omitempty"`
 }
 
+type DeploymentStatus string
+
+const (
+	DeploymentNotInstalled DeploymentStatus = "NotInstalled"
+	DeploymentNotReady     DeploymentStatus = "NotReady"
+	DeploymentRunning      DeploymentStatus = "Running"
+)
+
 // KeplerInternalStatus represents status of KeplerInternal
 type KeplerInternalStatus struct {
-	Exporter ExporterStatus `json:"exporter,omitempty"`
+	Exporter    ExporterStatus    `json:"exporter,omitempty"`
+	Estimator   EstimatorStatus   `json:"estimator,omitempty"`
+	ModelServer ModelServerStatus `json:"modelServer,omitempty"`
+}
+
+type EstimatorStatus struct {
+	Status DeploymentStatus `json:"status,omitempty"`
+}
+
+type ModelServerStatus struct {
+	Status DeploymentStatus `json:"status,omitempty"`
 }
 
 func (ki KeplerInternal) Namespace() string {
@@ -92,6 +178,10 @@ func (ki KeplerInternal) Namespace() string {
 
 func (ki KeplerInternal) DaemonsetName() string {
 	return ki.Name
+}
+
+func (ki KeplerInternal) ModelServerDeploymentName() string {
+	return ki.Name + "-model-server"
 }
 
 func (ki KeplerInternal) ServiceAccountName() string {
